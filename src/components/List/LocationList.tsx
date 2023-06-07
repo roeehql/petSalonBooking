5c11eb6f508f8a4f70fc1e7e9b0494ac81a615af
taei <roeehql@naver.com>
2023-06-08 02:25:55 +0900

    feat: 지역별 보기 기능 구현

--- src/components/List/LocationList.tsx
+++ src/components/List/LocationList.tsx
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import locationList from "./data/locationData.json";
 
-const LocationList = () => {
-  const [locationCode, setLocationCode] = useState(0);
-
+const LocationList = ({
+  locationCode,
+  onSetLocationCode,
+}: {
+  locationCode: number;
+  onSetLocationCode: (code: number) => void;
+}) => {
   return (
-    <ol className="grid grid-cols-4 mt-10">
+    <ol className="grid grid-cols-4 mt-6">
       {locationList.map((location) => (
         <li
           key={location.code}
-          onClick={() => setLocationCode(location.code)}
+          onClick={() => onSetLocationCode(location.code)}
           className={`list-none min-w-fit px-6 py-3 m-3 text-center text-green-900 text-base tracking-tighter font-medium border-2 border-gray-300 cursor-pointer last:border-r-2 hover:bg-green-600 hover:text-white ${
             location.code === locationCode && "bg-green-600 text-white"
           }`}
--- src/components/List/PetSalonList.tsx
+++ src/components/List/PetSalonList.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useGetSalonList } from "./hooks/useGetSalonList";
 import SalonItem from "./SalonItem";
 import LocationList from "./LocationList";
+import { SalonList } from "types/ListTypes";
+import { TitleH } from "components/reservation/MyReservationList";
 
 const PetSalonList = () => {
+  const [locationCode, setLocationCode] = useState(0);
   const { salonList } = useGetSalonList();
+  const [selectedList, setSelectedList] = useState<SalonList[]>([]);
+
+  const displaySalonList = () => {
+    const selectedSalons = salonList.filter(
+      (salon) => parseInt(salon.address.charAt(0)) === locationCode
+    );
+    setSelectedList(selectedSalons);
+  };
+
+  useEffect(() => {
+    displaySalonList();
+  }, [locationCode]);
+
   return (
-    <div className="flex flex-col justify-center items-center w-full min-h-screen h-fit">
-      <LocationList />
+    <div className="flex flex-col justify-start items-center w-full min-h-screen h-fit">
+      <TitleH text="✅지역을 선택해주세요!" />
+      <LocationList
+        locationCode={locationCode}
+        onSetLocationCode={(code: number) => setLocationCode(code)}
+      />
       <ul className=" grid grid-cols-3">
-        {salonList.slice(0, 20).map((salon) => (
+        {selectedList.map((salon) => (
           <SalonItem key={salon.id} item={salon} />
         ))}
       </ul>
